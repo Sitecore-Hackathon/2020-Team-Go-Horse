5a075f1294bc1ae93ef9edc00c7d7e5977da5420
Rodrigo Peplau <rpeplau@nishtechinc.com>
2020-02-29 11:34:56 -0300

    Search Modules finished

---
+++ src/Feature/SitecoreModule/code/Models/Search/AdvancedResultItem.cs
@@ -0,0 +1,11 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.SearchTypes;
+
+namespace Feature.SitecoreModule.Models.Search
+{
+    public class AdvancedResultItem : SearchResultItem
+    {
+        [IndexField("_templates")]
+        public string Templates { get; set; }
+    }
+}
